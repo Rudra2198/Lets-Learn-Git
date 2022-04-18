66df4767d7e964a69dd9723fbfd67175453184c0
Rudra Patel <patelrudra219@gmail.com>
2022-04-18 10:33:39 +0530

    inital commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class sum {
+    // write a program to add two numbers
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Sum of two numbers is " + c);
+    }
+
+}
