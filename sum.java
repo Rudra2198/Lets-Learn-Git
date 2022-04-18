3b67fbe613093630e0820d195b675db3656645d4
Rudra Patel <patelrudra219@gmail.com>
2022-04-18 10:50:00 +0530

    added feature which takes user input

--- sum.java
+++ sum.java
@@ -1,11 +1,23 @@
+import java.util.*;
+
 public class sum {
     // write a program to add two numbers
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(" Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.print(" Enter the second number: ");
+        int b = sc.nextInt();
+
         int c = a + b;
-        System.out.println("Sum of two numbers is " + c);
+        System.out.println("");
+        System.out.println(" Sum of two numbers is " + c);
+
+        sc.close();
     }
 
 }
