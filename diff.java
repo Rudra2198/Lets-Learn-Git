c213eb3e13955a8283dcd26e891637fcce75fd0c
Rudra Patel <patelrudra219@gmail.com>
2022-04-18 10:46:45 +0530

    adding diff.java

---
+++ diff.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class diff {
+    // write a program that takes two numbers as input and prints the difference
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print(" Enter the first number: ");
+        int a = sc.nextInt();
+
+        System.out.print(" Enter the second number: ");
+        int b = sc.nextInt();
+        int c = a - b;
+
+        System.out.println("");
+        System.out.println(" Difference of two numbers is " + c);
+
+        sc.close();
+    }
+
+}
