b9485d011f122a00d188f55b1629910db94f83e5
Rudra Patel <patelrudra219@gmail.com>
2022-05-27 15:12:09 +0530

    added a file that checks for the os name of the device

---
+++ getos.java
@@ -0,0 +1,9 @@
+public class getos {
+    public static void main(String[] args) {
+
+        // get the name of operating system
+        String operatingSystem = System.getProperty("os.name");
+        System.out.println(operatingSystem);
+
+    }
+}
